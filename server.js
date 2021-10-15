327071a72dbc7553c816ec64925399a93b76e1d4
Gail Tyler <gtyler11@gmail.com>
2021-10-15 16:19:51 -0700

    Set up basic DB file structure, including server.js format

---
+++ server.js
@@ -0,0 +1,39 @@
+/* === External Modules === */
+
+
+/* === Internal Modules === */
+
+// -- DB Connection --
+
+
+/* === System Variables === */
+
+
+/* === System Configuration === */
+
+// -- Tell Express we are using ejs --
+
+
+// -- Make public directory available --
+
+
+// -- Express use body data (form data) --
+
+
+// -- Override request methods --
+
+
+/* === Middleware === */
+
+// -- Logger --
+
+
+/* === Routes === */
+
+// -- Default Routes --
+
+
+// -- Utility Routes --
+
+/* === Server Listener === */
+
