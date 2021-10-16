761f630bd409a439410f32ea6175b1804a82b663
truengsuksilp <truengsuksilp@gmail.com>
2021-10-16 11:16:51 -0700

    Setup MongoDB connection - local on  config/db.connection

--- config/db.connection.js
+++ config/db.connection.js
@@ -1,9 +1,23 @@
 /* === External Modules === */
+const mongoose = require('mongoose');
 
 /* === System Variables === */
+const MONGODB_URI = 'mongodb://localhost:27017';
+
 
 /* === Connect & Log === */
+mongoose.connect(MONGODB_URI);
 
-// connection.on('connected')
-// connection.on('error')
-// connectio.on('disconnected')
+mongoose.connection.on('connected', () => {
+    console.log(`Connected: ${MONGODB_URI} 🦇🦇🦇`);
+});
+
+mongoose.connection.on('error', (error) => {
+    console.log(`Connection err: ${MONGODB_URI} 😭`);
+    console.log(error);
+});
+
+mongoose.connection.on('disconnected', () => {
+    console.log(`Disconnected: ${MONGODB_URI}`);
+    console.log('byeee 👋');
+});
