8c4d13aea2416556a23ef2637744537b6e7d58d8
Gail Tyler <gtyler11@gmail.com>
2021-10-15 17:25:22 -0700

    updated db.connection with mongoose connection (at least partially)

--- config/db.connection.js
+++ config/db.connection.js
@@ -1,9 +1,18 @@
 /* === External Modules === */
+const mongoose = require("mongoose");
+
 
 /* === System Variables === */
+
+// == set mongoose connection with connection string
+const connectionString = "mongodb://localhost:29017/test";
 
 /* === Connect & Log === */
 
 // connection.on('connected')
+mongoose.connection.on("connected", function () {
+    console.log("Mongoose connected to ${connectionString}")
+})
+
 // connection.on('error')
 // connectio.on('disconnected')
